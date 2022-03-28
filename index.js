f109bd1969e8ee013f7ed3a7c1de839816676fd4
Alexander Edwards <alexd.edwards1999@gmail.com>
2022-03-28 15:18:51 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    for (const wins of record) {
+        if (wins.result === "W") {
+            return(wins.year);
+        };
+    };
+};
+
